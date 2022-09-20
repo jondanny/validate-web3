113141fc6a06574400a4f7943528bbb51a54558a
jondanny <jdnguyen928@gmail.com>
2022-09-20 05:43:01 -0700

    add microservice event pattern

--- src/web3/web3.controller.ts
+++ src/web3/web3.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 import { Web3Service } from './web3.service';
 import { Wallet } from './types/wallet.interface';
 
@@ -6,16 +7,19 @@ import { Wallet } from './types/wallet.interface';
 export class Web3Controller {
   constructor(private readonly web3Service: Web3Service) {}
 
+  @EventPattern('got_balance')
   @Get('balance')
   async getBalance(@Query('address') address: string): Promise<number> {
     return this.web3Service.getMaticBalance(address);
   }
 
+  @EventPattern('wallet_created')
   @Post('create-wallet')
   async createWallet(): Promise<Wallet> {
     return this.web3Service.createWallet();
   }
 
+  @EventPattern('initial_matic_sent')
   @Post('send-initial-matic')
   async sendInitialMatic(
     @Body('address') address: string,
@@ -24,6 +28,7 @@ export class Web3Controller {
     return this.web3Service.sendInitialMatic(address, amount);
   }
 
+  @EventPattern('minted')
   @Post('mint')
   async mint(
     @Body('metadataUri') metadataUri: string,
@@ -32,11 +37,13 @@ export class Web3Controller {
     return this.web3Service.mint(metadataUri, amount);
   }
 
+  @EventPattern('burned')
   @Post('burn')
   async burn(@Body('tokenId') tokenId: number, @Body('amount') amount: number) {
     return this.web3Service.burn(tokenId, amount);
   }
 
+  @EventPattern('bought_from_primary')
   @Post('buy-from-primary')
   async buyFromPrimaryMarket(
     @Body('buyer') buyer: string,
@@ -46,6 +53,7 @@ export class Web3Controller {
     return this.web3Service.buyFromPrimaryMarket(buyer, tokenId, amount);
   }
 
+  @EventPattern('put_on_sale')
   @Post('put-on-sale')
   async putOnSale(
     @Body('privateKey') privateKey: string,
@@ -56,6 +64,7 @@ export class Web3Controller {
     return this.web3Service.putOnSale(privateKey, tokenId, amount, price);
   }
 
+  @EventPattern('put_off_sale')
   @Post('put-off-sale')
   async putOffSale(
     @Body('privateKey') privateKey: string,
@@ -64,7 +73,8 @@ export class Web3Controller {
     return this.web3Service.putOffSale(privateKey, itemId);
   }
 
-  @Post('changePrice')
+  @EventPattern('price_changed')
+  @Post('change-price')
   async changePrice(
     @Body('privateKey') privateKey: string,
     @Body('itemId') itemId: number,
@@ -73,6 +83,7 @@ export class Web3Controller {
     return this.web3Service.changePrice(privateKey, itemId, price);
   }
 
+  @EventPattern('bought_from_secondary')
   @Post('buy-from-secondary')
   async buyFromSecondaryMarket(
     @Body('privateKey') privateKey: string,
