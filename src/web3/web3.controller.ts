af432ec53fa88ed1e6a8101927c68aadcb93eb8a
jondanny <jdnguyen928@gmail.com>
2022-09-20 05:01:54 -0700

    add web3 endpoints

--- src/web3/web3.controller.ts
+++ src/web3/web3.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { Web3Service } from './web3.service';
+import { Wallet } from './types/wallet.interface';
 
 @Controller('web3')
 export class Web3Controller {
@@ -10,11 +11,74 @@ export class Web3Controller {
     return this.web3Service.getMaticBalance(address);
   }
 
+  @Post('create-wallet')
+  async createWallet(): Promise<Wallet> {
+    return this.web3Service.createWallet();
+  }
+
   @Post('send-initial-matic')
   async sendInitialMatic(
     @Body('address') address: string,
     @Body('amount') amount: number,
   ): Promise<string | void> {
     return this.web3Service.sendInitialMatic(address, amount);
+  }
+
+  @Post('mint')
+  async mint(
+    @Body('metadataUri') metadataUri: string,
+    @Body('amount') amount: number,
+  ) {
+    return this.web3Service.mint(metadataUri, amount);
+  }
+
+  @Post('burn')
+  async burn(@Body('tokenId') tokenId: number, @Body('amount') amount: number) {
+    return this.web3Service.burn(tokenId, amount);
+  }
+
+  @Post('buy-from-primary')
+  async buyFromPrimaryMarket(
+    @Body('buyer') buyer: string,
+    @Body('tokenId') tokenId: number,
+    @Body('amount') amount: number,
+  ): Promise<void> {
+    return this.web3Service.buyFromPrimaryMarket(buyer, tokenId, amount);
+  }
+
+  @Post('put-on-sale')
+  async putOnSale(
+    @Body('privateKey') privateKey: string,
+    @Body('tokenId') tokenId: number,
+    @Body('amount') amount: number,
+    @Body('price') price: number,
+  ): Promise<number> {
+    return this.web3Service.putOnSale(privateKey, tokenId, amount, price);
+  }
+
+  @Post('put-off-sale')
+  async putOffSale(
+    @Body('privateKey') privateKey: string,
+    @Body('itemId') itemId: number,
+  ): Promise<void> {
+    return this.web3Service.putOffSale(privateKey, itemId);
+  }
+
+  @Post('changePrice')
+  async changePrice(
+    @Body('privateKey') privateKey: string,
+    @Body('itemId') itemId: number,
+    @Body('price') price: number,
+  ): Promise<void> {
+    return this.web3Service.changePrice(privateKey, itemId, price);
+  }
+
+  @Post('buy-from-secondary')
+  async buyFromSecondaryMarket(
+    @Body('privateKey') privateKey: string,
+    @Body('itemId') itemId: number,
+    @Body('price') price: number,
+  ): Promise<void> {
+    return this.web3Service.buyFromSecondaryMarket(privateKey, itemId, price);
   }
 }
